645131f151caf5e1feb911a04280ae83d5e42204
Cong <congve1@live.com>
2017-05-13 16:01:05 +0800

    modify the index page and add the js css files

---
+++ static/js/upload-image.js
@@ -0,0 +1,82 @@
+/**
+ * Created by clw on 17-5-13.
+ */
+function noPreview() {
+  $('#image-preview-div').css("display", "none");
+  $('#preview-img').attr('src', 'noimage');
+  $('upload-button').attr('disabled', '');
+}
+
+function selectImage(e) {
+  $('#file').css("color", "green");
+  $('#image-preview-div').css("display", "block");
+  $('#preview-img').attr('src', e.target.result);
+  $('#preview-img').css('max-width', '550px');
+}
+
+$(document).ready(function (e) {
+
+  var maxsize = 500 * 1024; // 500 KB
+
+
+  $('#upload-image-form').on('submit', function(e) {
+
+    e.preventDefault();
+
+    $('#message').empty();
+    $('#loading').show();
+
+    $.ajax({
+      url: "segimage",
+      type: "POST",
+      data: new FormData(this),
+      contentType: false,
+      cache: false,
+      processData: false,
+      success: function(data)
+      {
+          $("#loading").removeClass("progress");
+          $("#loading").addClass("alert alert-success");
+          $("#loading").text("图片上传成功");
+          $("#loading").fadeOut(1000,function(){
+          $("body").html(data)
+        });
+      }
+    });
+
+  });
+
+  $('#file').change(function() {
+
+    $('#message').empty();
+
+    var file = this.files[0];
+    var match = ["image/jpeg", "image/png", "image/jpg"];
+
+    if ( !( (file.type == match[0]) || (file.type == match[1]) || (file.type == match[2]) ) )
+    {
+      noPreview();
+
+      $('#message').html('<div class="alert alert-warning" role="alert">不支持的图片格式. 仅支持:JPG, JPEG, PNG.</div>');
+
+      return false;
+    }
+
+    if ( file.size > maxsize )
+    {
+      noPreview();
+
+      $('#message').html('<div class=\"alert alert-danger\" role=\"alert\">The size of image you are attempting to upload is ' + (file.size/1024).toFixed(2) + ' KB, maximum size allowed is ' + (maxsize/1024).toFixed(2) + ' KB</div>');
+
+      return false;
+    }
+
+    $('#upload-button').removeAttr("disabled");
+
+    var reader = new FileReader();
+    reader.onload = selectImage;
+    reader.readAsDataURL(this.files[0]);
+
+  });
+
+});
