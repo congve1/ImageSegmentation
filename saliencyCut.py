3909f6348d0839f8b18cc1c92eef0c236ad56cf7
Cong <congve1@live.com>
2017-05-23 19:20:28 +0800

    add bouding box

--- saliencyCut.py
+++ saliencyCut.py
@@ -9,10 +9,23 @@ def processImage(filename):
     img3f *= 1. / 255
     sal = SaliencyRC.GetHC(img3f)
     #idxs = np.where(sal < (sal.max() + sal.min()) / 4.96)
-    img3i[np.where(sal < (sal.max() + sal.min())/4)] = 0
+    #img3i[np.where(sal < (sal.max() + sal.min())/4)] = 0
+    idxs = np.where(sal >= (sal.max() + sal.min()) / 2)
+    I = [np.min(idxs[0]),np.max(idxs[0])]
+    J = [np.min(idxs[1]),np.max(idxs[1])]
+    imgBBox = img3i.copy()
+    imgBBox[I[0]:I[0]+1,J[0]:J[1]+1] = [255,0,0]
+    imgBBox[I[0]:I[1]+1,J[1]:J[1]+1] = [255,0,0]
+    imgBBox[I[1]:I[1]+1,J[0]:J[1]+1] = [255,0,0]
+    imgBBox[I[0]:I[1]+1,J[0]:J[0]+1] = [255,0,0]
+    resImg = np.zeros(img3i.shape,img3i.dtype)
+    resImg[I[0]:I[1],J[0]:J[1]] = img3i[I[0]:I[1],J[0]:J[1]]
+    resImg[np.where(sal < (sal.max() + sal.min())/ 4)] = 0
     sal *= 255
-    cv2.imwrite("{}process.jpg".format(filename),img3i)
+    cv2.imwrite("{}process.jpg".format(filename),resImg)
+    cv2.imwrite("{}bbox.jpg".format(filename),imgBBox)
     cv2.imwrite("{}hc.jpg".format(filename),sal.astype(np.int16))
+
 
 
 def processImage1(filename):
